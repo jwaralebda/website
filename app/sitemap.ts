f9bcbf8ea132dbe775c236a02e196c8500e44e61
Sheikh Shahil <shahil@indicsoft.com>
2026-02-15 21:36:00 +0530

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+ return [
+  {
+    url: "https://www.jwaralebdaa.com/",
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 1,
+  },
+  {
+    url: "https://www.jwaralebdaa.com/About",
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.8,
+  },
+  {
+    url: "https://www.jwaralebdaa.com/contact",
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.8,
+  },
+];
+
+}
